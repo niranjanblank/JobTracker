046ee1525921212763efb52ac994d6c8fb498a8f
Niranjan Shah <niranjanshah474@gmail.com>
2025-03-04 22:24:34 +1100

    interface and generic repository created which will be used by all the repositories

---
+++ backend/JobTracker/Repositories/GenericRepository.cs
@@ -0,0 +1,60 @@
+using Microsoft.EntityFrameworkCore;
+using JobTracker.Data;
+using System.Linq.Expressions;
+
+namespace JobTracker.Repositories
+{
+    public class GenericRepository<T>: IGenericRepository<T> where T: class
+    {
+        private readonly JobDbContext _context;
+        pirvate readonly DBSet<T> _dbSet;
+
+        public GenericRepository(JobDbContext context)
+        {
+            _context = context;
+            _dbSet = _context.Set<T>();
+        }
+
+        public async Task<IEnumerable<T>> GetAllAsync()
+        {
+            return await _dbSet.ToListAsync();
+        }
+
+        public async Task<T?> GetByIdAsync(int id)
+        {
+            return await _dbSet.FindAsync(id);
+        }
+
+        public async Task<T> AddAsync(T entity)
+        {
+            _dbSet.Add(entity);
+            await _context.SaveChangesAsync();
+            return entity;
+        }
+
+        public async Task<Bool> DeleteAsync(int id)
+        {
+            var entity = await _dbSet.FindAsync(id);
+            if (entity == null)
+            {
+                return false;
+            }
+
+            _dbSet.Remove(entity);
+            await _context.SaveChangesAsync();
+            return true;
+        }
+
+        public async Task<T> UpdateAsync(T entity)
+        {
+            _dbSet.Update(entity);
+            await _context.SaveChangesAsync();
+            return entity;
+        }
+
+        public async Task<IEnumerable<T>> FindAsync(Expression<Func<T, bool>> predicate)
+        {
+            return await _dbSet.Where(predicate).ToListAsync();
+        }
+    }
+}
---
+++ backend/JobTracker/Repositories/IGenericRepository.cs
@@ -0,0 +1,14 @@
+using System.Linq.Expressions;
+
+namespace JobTracker.Repositories
+{
+    public interface IGenericRepository<T> where T : class
+    {
+        Task<IEnumerable<T>> GetAllAsync();
+        Task<T?> GetByIdAsync(int id);
+        Task<T> AddAsync(T entity);
+        Task<bool> DeleteAsync(int id);
+        Task<T> UpdateAsync(T entity);
+        Task<IEnumerable<T>> FindAsync(Expression<Func<T, bool>> predicate);
+    }
+}
